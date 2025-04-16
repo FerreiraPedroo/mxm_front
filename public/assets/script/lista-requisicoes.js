0d6f035d0f969148187859cd7bb039771505bf34
FerreiraPedroo <pedroohenrique@hotmail.com>
2025-04-16 18:26:14 -0300

    update

--- public/assets/script/lista-requisicoes.js
+++ public/assets/script/lista-requisicoes.js
@@ -1,3 +1,7 @@
+function mudarPagina(pagina) {
+
+}
+
 async function carregarRequisicoes(posicao = 0) {
  const reqLista = await API.fetchDados("/requisicoes", "GET");
  const items = document.getElementById("items");
@@ -6,9 +10,10 @@ async function carregarRequisicoes(posicao = 0) {
   items.innerHTML = "";
    reqLista.dados.forEach((requisicao) => {
     items.innerHTML += `
-     <div class="item-box" data-id="${requisicao.req}">
+     <div class="item-box" onclick="location.replace('/requisicao.html?reqID=${requisicao.id}');" data-id="${requisicao.req}">
       <div class="item item-requisicao">${requisicao.req}</div>
-      <div class="item item-centro-custo">${requisicao.centro_custo}</div>
+      <div class="item item-centro-custo">${requisicao.centro_custo.codigo + " - " +requisicao.centro_custo.nome + " - " + requisicao.centro_custo.unidade}</div>
+      <div class="item item-justificativa">${requisicao.observacao}</div>
       <div class="item item-justificativa">${requisicao.justificativa}</div>
      </div>
     `;
